6390106eb469784e20464ed719c62ac4061ce991
gabrielabiah <gabriel@manifestghana.com>
2025-07-27 17:53:53 +0000

    Replaced a tag with Link tag from next/link

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 const Header: React.FC = () => {
 	return (
 		<header className="flex justify-between py-4 bg-white shadow-md px-6">
 			<h1>My Application</h1>
 			<nav>
 				<ul className="flex space-x-4">
-					<li><a href="/home">Home</a></li>
-					<li><a href="/about">About</a></li>
+					<li><Link href="/home">Home</Link></li>
+					<li><Link href="/about">About</Link></li>
 				</ul>
 			</nav>
 		</header>
