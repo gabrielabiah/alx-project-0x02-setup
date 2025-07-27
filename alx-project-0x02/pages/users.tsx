d33d20e7ef9b2f7580e41efe5fc7e30eb94c7a93
gabrielabiah <gabriel@manifestghana.com>
2025-07-27 23:02:31 +0000

    added users pages with api data

---
+++ alx-project-0x02/pages/users.tsx
@@ -0,0 +1,42 @@
+import { GetStaticProps } from "next";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
+import Header from "@/components/layout/Header";
+
+interface UsersPageProps {
+	users: UserProps[];
+}
+
+const UsersPage = ({ users }: UsersPageProps) => {
+	return (
+		<>
+			<Header />
+			<main className="p-4">
+				<h1 className="text-2xl font-bold mb-4">Users</h1>
+				{users.map(user => (
+					<UserCard
+						key={user.id}
+						id={user.id}
+						name={user.name}
+						email={user.email}
+						address={user.address}
+					/>
+				))}
+			</main>
+		</>
+	);
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const users: UserProps[] = await res.json();
+
+	return {
+		props: {
+			users,
+		},
+		revalidate: 3600, // Rebuild the static page every 1 hour (optional)
+	};
+};
+
+export default UsersPage;
